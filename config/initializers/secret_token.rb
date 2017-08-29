94015552f2eba8133e5c2b4c171a9ffd02b63220
John Baycroft <jbaycroft@capaccio.com>
2017-08-28 17:45:28 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+HealthTracker::Application.config.secret_key_base = '2ac843fcbd89631b696d38d72bada770fe309dc0e10c32b0725ac16d84ff8c2abecd3eb950b6dbfb4ffc13f825319861b8438f2695471cb61dfb0cc30eff9ea3'
